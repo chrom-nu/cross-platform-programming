ee4b692fabb7ec49ac71735c952d8c39e01b8d51
chrom-nu <ANDRII.KHROMOVYCH@lnu.edu.ua>
2026-09-12 15:06:04 +0300

    lab01: solution CrossApp, Cli, вибір домену

---
+++ src/Cli/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.InteropServices;
+
+
+Console.WriteLine("CrossApp – практикум з крос-платформного програмування");
+Console.WriteLine("Студент: Хромович Андрій, група ФЕІ-32с");
+
+Console.WriteLine(new string('-', 52));
+
+Console.WriteLine($"ОС (OSDescription) : {RuntimeInformation.OSDescription}");
+Console.WriteLine($"ОС (Environment) : {Environment.OSVersion}");
+Console.WriteLine($"Архітектура процесу : {RuntimeInformation.ProcessArchitecture}");
+Console.WriteLine($"Версія .NET (CLR) : {Environment.Version}");
+Console.WriteLine($"Runtime : {RuntimeInformation.FrameworkDescription}");
+Console.WriteLine($"Каталог застосунку : {AppContext.BaseDirectory}");
+Console.WriteLine($"Поточний каталог : {Environment.CurrentDirectory}");
+
+Console.WriteLine(new string('-', 52));
+
+Console.WriteLine("Предметна область: Бібліотека. Сутності: Book (видання), BookCopy (примірник), Reader (читач), Loan (видача).");
