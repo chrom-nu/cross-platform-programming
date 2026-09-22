171e57f31c768eb844e0069729f781ee0a45a19b
chrom-nu <ANDRII.KHROMOVYCH@lnu.edu.ua>
2026-09-22 12:42:18 +0300

    lab2

--- src/Cli/Program.cs
+++ src/Cli/Program.cs
@@ -1,34 +1,12 @@
-﻿using System.Runtime.InteropServices;
-using Core;
-using System.Text;
+﻿using Core;
 
-Console.OutputEncoding = Encoding.UTF8;
-
-Console.WriteLine("CrossApp – практикум з крос-платформного програмування");
-Console.WriteLine("Студент: Хромович Андрій, група ФЕІ-32с");
-Console.WriteLine("CrossApp - Практика з крос-платформного програмування");
-Console.WriteLine("Студент: Хромій Юрій, група КП-32мп");
-
-Console.WriteLine(new string('-', 52));
 EnvironmentReport report = EnvironmentInfo.Collect();
 
-Console.WriteLine($"ОС (OSDescription) : {RuntimeInformation.OSDescription}");
-Console.WriteLine($"ОС (Environment) : {Environment.OSVersion}");
-Console.WriteLine($"Архітектура процесу : {RuntimeInformation.ProcessArchitecture}");
-Console.WriteLine($"Версія .NET (CLR) : {Environment.Version}");
-Console.WriteLine($"Runtime : {RuntimeInformation.FrameworkDescription}");
-Console.WriteLine($"Каталог застосунку : {AppContext.BaseDirectory}");
-Console.WriteLine($"Поточний каталог : {Environment.CurrentDirectory}");
-
+Console.WriteLine("CrossApp – інформація про середовище");
 Console.WriteLine(new string('-', 52));
-Console.WriteLine($"ОС (OSDescription) : {report.OsDescription}");
-Console.WriteLine($"Архітектура        : {report.ProcessArchitecture}");
-Console.WriteLine($"Runtime            : {report.FrameworkDescription}");
-Console.WriteLine($"RID (визначено)    : {report.DetectedRid}");
-Console.WriteLine($"RID (від .NET)     : {report.ReportedRid}");
-Console.WriteLine($"Каталог            : {report.BaseDirectory}");
-Console.WriteLine($"Примітка збірки    : {report.BuildNote}");
-Console.WriteLine(new string('-', 52));
-
-Console.WriteLine("Предметна область: Бібліотека. Сутності: Book (видання), BookCopy (примірник), Reader (читач), Loan (видача).");
-Console.WriteLine("Предметна область: Бібліотека. Сутності: Book (Книга), BookCopy (Примірник), Reader (Читач), Loan (Позика).");
+Console.WriteLine($"ОС : {report.OsDescription}");
+Console.WriteLine($"Runtime : {report.FrameworkDescription}");
+Console.WriteLine($"Архітектура : {report.ProcessArchitecture}");
+Console.WriteLine($"RID (визначено): {report.DetectedRid}");
+Console.WriteLine($"RID (від .NET) : {report.ReportedRid}");
+Console.WriteLine($"Каталог : {report.BaseDirectory}");Console.WriteLine($"Примітка збірки: {report.BuildNote}");
